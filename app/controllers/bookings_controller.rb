f916dfd60d95f4fe5989a35292cf6a86180d053a
KS <sobieraj.k.2@gmail.com>
2017-04-07 17:31:31 +0200

    create scaffolding for Rentals and Bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,43 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :update, :destroy]
+
+  # GET /bookings
+  def index
+    @bookings = Booking.all
+    json_response(@bookings)
+  end
+
+  # POST /bookings
+  def create
+    @booking = Booking.create!(booking_params)
+    json_response(@booking, :created)
+  end
+
+  # GET /bookings/:id
+  def show
+    json_response(@booking)
+  end
+
+  # PUT /bookings/:id
+  def update
+    @booking.update(booking_params)
+    head :no_content
+  end
+
+  # DELETE /bookings/:id
+  def destroy
+    @booking.destroy
+    head :no_content
+  end
+
+  private
+
+  def booking_params
+    # whitelist params
+    params.permit(:start_at, :end_at, :client_email, :price, :rental_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+end
