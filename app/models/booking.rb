68495a098cad1b070a528f445cc301416b0ffa7c
KS <sobieraj.k.2@gmail.com>
2017-04-07 20:00:06 +0200

    ensure that booking period is at least one night in duration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :rental
 
   validate :price_is_valid
+  validate :at_least_one_night_stay
   validate :period_do_not_overlap_existing_ones
 
   scope :overlaping_periods, -> booking {
@@ -15,10 +16,16 @@ class Booking < ApplicationRecord
       end
     end
 
+    def at_least_one_night_stay
+      if (self.end_at.to_date - self.start_at.to_date).to_i < 1
+        errors.add(:duration_of_a_period, "specified by start_at and end_at parameters cannot be less than one night stay")
+      end
+    end
+
     def period_do_not_overlap_existing_ones
       overlaps = self.rental.bookings.overlaping_periods(self)
       unless overlaps.empty?
-        errors.add(:period, 'specified by start_at and end_at attributes is not available for booking')
+        errors.add(:period, 'specified by start_at and end_at parameters is not available for booking')
       end
     end
 end
