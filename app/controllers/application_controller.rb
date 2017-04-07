9b55806571f6719e1dc88657783fe3f0237ea49a
KS <sobieraj.k.2@gmail.com>
2017-04-07 21:58:19 +0200

    require a simple token authentication for every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  before_filter :authenticate_from_token!
+
+  def authenticate_from_token!
+    token = params[:api_token]
+    if !token || !Devise.secure_compare(Rails.application.secrets.api_token, token)
+      render nothing: true, status: :unauthorized
+    end
+  end
 end
