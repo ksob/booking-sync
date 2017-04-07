1f4dfb2578f4f79ef5814acae0e3f42805501255
KS <sobieraj.k.2@gmail.com>
2017-04-07 19:34:56 +0200

    ensure that price is valid when creating a new booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :rental
+
+  validate :price_is_valid
+
+  private
+    def price_is_valid
+      if self.price != (self.end_at.to_date - self.start_at.to_date).to_i * self.rental.daily_rate
+        errors.add(:price, "should be valid")
+      end
+    end
 end
